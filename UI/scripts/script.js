8bcafa006e74bc4ff36b190e78f0ed0c3ec787c1
Ilori Ezekiel <Ilorieazykiel@gmail.com>
2019-02-27 06:24:23 +0100

    Design UI for composing and messaging an individual

--- UI/scripts/script.js
+++ UI/scripts/script.js
@@ -12,18 +12,51 @@ window.onload= function ready(){
     if(document.querySelector("a[href='#Inbox']")){ //if on dashboard page -> inbox.html
         /** Right-panel-Menus Event **/
         document.querySelector("a[href='#Inbox']").onclick = (event) => {
+            
             document.querySelector(".right-inbox ").classList.remove("hidden");
             document.querySelector(".view-message").classList.add("hidden");
-           
+            document.querySelector(".right-compose").classList.add("hidden");
+            
+            document.querySelector("a[href='#Inbox']").classList.add('active');
+            document.querySelector("a[href='#Compose']").classList.remove('active');
         }
+        document.querySelector("a[href='#Compose']").onclick = (event) =>{
+            document.querySelector(".right-compose").classList.remove("hidden");
+            document.querySelector(".right-inbox ").classList.add("hidden");
+            document.querySelector(".view-message").classList.add("hidden");
+            
+            document.querySelector("a[href='#Inbox']").classList.remove('active');
+            document.querySelector("a[href='#Compose']").classList.add('active');
+        }
+        
         
         document.querySelectorAll(".inbox .bottom .inbox-view >div >*:not(input)").forEach((element) => {
             element.onclick = (event) => {
                 document.querySelector(".right-inbox ").classList.add("hidden");
+                document.querySelector(".right-compose").classList.add("hidden");
                 document.querySelector(".view-message").classList.remove("hidden");
             }
         })
-        
+        document.querySelector("[name='sendMail']").onsubmit = (event) => {
+            let sendButton = document.querySelector("[name='sendMail'] button");
+            sendButton.innerHTML = 'SENDING';
+            sendButton.classList.add("sending");
+            
+            setTimeout(() => {
+                sendButton.innerHTML = "SENT";
+                sendButton.classList.add("sent");
+            }, 2000);
+            setTimeout(() => {
+                
+                sendButton.innerHTML = "SEND"
+                sendButton.classList.remove("sending");
+                sendButton.classList.remove("sent");
+                document.querySelector("[name='sendMail']").reset();
+            }, 3000);
+            
+            return false;
+        }
 
     }
-}
+    
+};
