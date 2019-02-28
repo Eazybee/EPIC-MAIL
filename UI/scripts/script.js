2d134a6ae32a734af1120aaf9b9ac5b2dc2d6902
Ilori Ezekiel <Ilorieazykiel@gmail.com>
2019-02-28 03:32:09 +0100

    Design UI for user to save mail as draft

--- UI/scripts/script.js
+++ UI/scripts/script.js
@@ -17,41 +17,50 @@ window.onload= function ready(){
                 element.classList.add("hidden");
             });
         }
-        /** Right-panel-Menus Event **/
+        /** Left-panel-Menus Event **/
         document.querySelector("a[href='#Inbox']").onclick = (event) => {
             
-            document.querySelector(".right-inbox ").classList.remove("hidden");
-            document.querySelector(".view-message").classList.add("hidden");
-            document.querySelector(".right-compose").classList.add("hidden");
-            document.querySelector(".right-sent").classList.add("hidden");
-            
+            document.querySelectorAll(".right:not(.right-inbox)").forEach((element) => {
+                element.classList.add("hidden");
+            });
+            document.querySelectorAll(".inbox .bottom .left> ul >li a:not([href='#Inbox'])").forEach((element) => {
+                element.classList.remove('active');
+            });
             document.querySelector("a[href='#Inbox']").classList.add('active');
-            document.querySelector("a[href='#Compose']").classList.remove('active');
-            document.querySelector("a[href='#Sent']").classList.remove('active');
+            document.querySelector(".right-inbox").classList.remove("hidden");
         }
-        document.querySelector("a[href='#Compose']").onclick = (event) =>{
-            document.querySelector(".right-compose").classList.remove("hidden");
-            document.querySelector(".right-inbox ").classList.add("hidden");
-            document.querySelector(".view-message").classList.add("hidden");
-            document.querySelector(".right-sent").classList.add("hidden");
-            
-            document.querySelector("a[href='#Inbox']").classList.remove('active');
+        document.querySelector("a[href='#Compose']").onclick = (event) => {
+            document.querySelectorAll(".right:not(.right-compose)").forEach((element) => {
+                element.classList.add("hidden");
+            });
+            document.querySelectorAll(".inbox .bottom .left> ul >li a:not([href='#Compose'])").forEach((element) => {
+                element.classList.remove('active');
+            });
             document.querySelector("a[href='#Compose']").classList.add('active');
-            document.querySelector("a[href='#Sent']").classList.remove('active');
+            document.querySelector(".right-compose").classList.remove("hidden");
         }
-        
-        document.querySelector("a[href='#Sent']").onclick = (event) =>{
-            document.querySelector(".right-compose").classList.add("hidden");
-            document.querySelector(".right-inbox ").classList.add("hidden");
-            document.querySelector(".view-message").classList.add("hidden");
-            document.querySelector(".right-sent").classList.remove("hidden");
-            
-            document.querySelector("a[href='#Inbox']").classList.remove('active');
-            document.querySelector("a[href='#Compose']").classList.remove('active');
+        document.querySelector("a[href='#Sent']").onclick = (event) => {
+            document.querySelectorAll(".right:not(.right-sent)").forEach((element) => {
+                element.classList.add("hidden");
+            });
+            document.querySelectorAll(".inbox .bottom .left> ul >li a:not([href='#Sent'])").forEach((element) => {
+                element.classList.remove('active');
+            });
             document.querySelector("a[href='#Sent']").classList.add('active');
+            document.querySelector(".right-sent").classList.remove("hidden");
+        }
+        document.querySelector("a[href='#Draft']").onclick = (event) => {
+            document.querySelectorAll(".right:not(.right-draft)").forEach((element) => {
+                element.classList.add("hidden");
+            });
+            document.querySelectorAll(".inbox .bottom .left> ul >li a:not([href='#Draft'])").forEach((element) => {
+                element.classList.remove('active');
+            });
+            document.querySelector("a[href='#Draft']").classList.add('active');
+            document.querySelector(".right-draft").classList.remove("hidden");
         }
         
-        document.querySelectorAll(".inbox .inbox-view >div >*:not(input)").forEach((element) => {
+        document.querySelectorAll(".right:not(.right-draft) .inbox-view >div >*:not(input)").forEach((element) => {
             element.onclick = (event) => {
                 document.querySelector(".right-inbox ").classList.add("hidden");
                 document.querySelector(".right-compose").classList.add("hidden");
@@ -59,7 +68,7 @@ window.onload= function ready(){
                 document.querySelector(".view-message").classList.remove("hidden");
             }
         })
-        
+        /**Send mail**/
         document.querySelector("[name='sendMail']").onsubmit = (event) => {
             let sendButton = document.querySelector("[name='sendMail'] button");
             sendButton.innerHTML = 'SENDING';
@@ -78,6 +87,40 @@ window.onload= function ready(){
             }, 3000);
             
             return false;
+        }
+        /** Save mail as draft **/
+        document.querySelector("#saveMail").onclick = (event) => {
+            let topic = document.querySelector('.inbox .right-compose .message input').value;
+            let msgBody = document.querySelector('.inbox .right-compose .message textarea').value;
+            let mailTo ="" ;
+            if(topic.trim() == "" && msgBody.trim() == ""){
+                alertMessage("Both subject and message body cannot be empty");
+            }else{
+                document.querySelectorAll(".inbox .right-compose .address span input[type='radio']").forEach((radioButton) => {
+                   if(radioButton.checked && radioButton.value == "Individual" ){
+                       mailTo = document.querySelector(".inbox .right-compose .address input[type='email']").value;
+                   }
+                    else if(radioButton.checked && radioButton.value == "Group" ){
+                        let selectElement = document.querySelector(".inbox .right-compose .address select");
+                        let selectedIndex = selectElement.selectedIndex; //geting index of selected option
+                        let options = selectElement.options; //getting collections of all options as an array
+                        mailTo = options[selectedIndex].value; //returning the value of selected option
+                   }
+                });
+                
+                let divElement = document.createElement('div');
+                divElement.innerHTML ='<input type="checkbox" value="1">'+
+                                    '<p>'+mailTo+'</p>'+
+                                    '<div>'+
+                                        '<p class="subject">'+topic+'</p>'+
+                                        '<p class="msg">s'+msgBody+'</p>'+
+                                    '</div>'+
+                                    '<label>'+new Date().getDate()+'-'+new Date().getMonth()+'</label>';
+            
+                document.querySelector('.right-draft .inbox-view').appendChild(divElement);
+                alertMessage("Message saved as draf succesfully");
+            }
+            
         }
 
         document.querySelector("#retract").onclick = (event) => { 
@@ -111,13 +154,14 @@ window.onload= function ready(){
                     selectGroup.classList.remove("hidden");
                     
                     (() => {
-                        selectGroup.innerHTML="<option disabled selected value='0'>Select Group</option>";
+                        selectGroup.innerHTML="<option disabled selected value=''>Select Group</option>";
                         document.querySelectorAll(".inbox .bottom ul.groups ul li a").forEach((element) => {
                             
-                            let name =element.innerHTML.split("»")[1];
+                            let name =element.innerHTML.split("» ")[1];
                             let option = document.createElement('option'); //create an option element(tag)
                             let groupName = document.createTextNode(name); //create a textnode 
                             option.appendChild(groupName); 			//add text to option tag created
+                            option.setAttribute("value", name); //set value = name
                             selectGroup.appendChild(option);	//add option to Select element
                         })
                         
