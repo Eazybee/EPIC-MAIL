1cd611b9f542224216481b97beea5e77883f0f07
Ilori Ezekiel <Ilorieazykiel@gmail.com>
2019-03-08 16:27:20 +0100

    feat(user): code /auth/signup post request to create new account
    - user should be able to create new account
    - should return a json object
    - should return status 201 if successful
    - should return status 400 if not successful
    
    [Delivers #164390087]

--- SERVER/v1/src/controller/RouteController.js
+++ SERVER/v1/src/controller/RouteController.js
@@ -1,3 +1,8 @@
+import Joi from 'joi';
+import jwt from 'jsonwebtoken';
+import users from '../model/Database';
+import User from '../model/User';
+
 class RouteController {
   static handleError(res, err, status = 400) {
     res.status(status).send({
@@ -11,7 +16,41 @@ class RouteController {
   }
 
   static signUp(req, res) {
-    return false;
+    const schema = Joi.object().keys({
+      email: Joi.string().required(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      password: Joi.string().required(),
+      rePassword: Joi.string().required(),
+    });
+    const { error } = Joi.validate(req.body, schema);
+    if (error) {
+      return RouteController.handleError(res, new Error(error.details[0].message), 400);
+    }
+    if (req.body.password !== req.body.rePassword) {
+      return res.status(400).json({
+        status: 400,
+        data: 'Password does not match',
+      });
+    }
+    const userExist = User.getUsers().some(user => user.getEmail() === req.body.email);
+    if (userExist) {
+      return res.status(400).json({
+        status: 400,
+        data: 'User with this email exist',
+      });
+    }
+
+
+    const newUser = new User(req.body.email,
+      req.body.firstName, req.body.lastName, req.body.password);
+    users.push(newUser);
+    return jwt.sign({ newUser }, 'Andela42', (err, token) => res.status(200).json({
+      status: 200,
+      data: [{
+        token,
+      }],
+    }));
   }
 
   static login(req, res) {
@@ -54,4 +93,5 @@ class RouteController {
     return false;
   }
 }
+RouteController.user = null;
 export default RouteController;
