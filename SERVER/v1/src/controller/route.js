b42595d088936bc32313cf9949f6664628d259d0
Ilori Ezekiel <Ilorieazykiel@gmail.com>
2019-03-11 00:02:38 +0100

    chore(endpoints): Reroute endpoints using appropriate verb
    - Reroute endpoints using appropriate verb
    
    [Chore #164524171]

--- SERVER/v1/src/controller/route.js
+++ SERVER/v1/src/controller/route.js
@@ -8,14 +8,17 @@ app.use(express.json());
 app.post('/api/v1/auth/signup', RouteController.signUp);
 app.post('/api/v1/auth/login', RouteController.login);
 app.post('/api/v1/messages', RouteController.validateLogin, RouteController.message);
+app.post('/api/v1/messages/save', RouteController.validateLogin, RouteController.saveDraft);
 
 //  GET
-app.get('/api/v1/messages', RouteController.validateLogin, RouteController.getInbox);
+app.get('/api/v1/messages', RouteController.validateLogin, RouteController.getInbox); //  Message
 app.get('/api/v1/messages/unread', RouteController.validateLogin, RouteController.getUnreadInbox);
 app.get('/api/v1/messages/read', RouteController.validateLogin, RouteController.getReadInbox);
 app.get('/api/v1/messages/sent', RouteController.validateLogin, RouteController.getSentMail);
 app.get('/api/v1/messages/:id', RouteController.validateLogin, RouteController.getMailId);
 
+//  PUT
+app.put('/api/v1/messages/', RouteController.validateLogin, RouteController.sendDraft);
 
 //  DELETE
 app.delete('/api/v1/messages/:id', RouteController.validateLogin, RouteController.deleteMail);
