50946631e42c3f67284ac469adf2edc2f0e860f6
Ilori Ezekiel <Ilorieazykiel@gmail.com>
2019-03-19 19:47:30 +0100

    chore(database): integrate previous endpoints to database
    - redirect endpoints to communicate with database
    - refactor unit test to work with database
    - re-direct test script to test database
    - add script to .travis.yml
    
    [Chore #164734799]

--- SERVER/src/Controller/MessageController.js
+++ SERVER/src/Controller/MessageController.js
@@ -1,116 +1,235 @@
 import UserController from './UserController';
-import Message from '../Model/Message';
+import db from '../Utility/Db';
+import Utility from '../Utility/Uitility';
 
 class MessageController {
   static sendMail(req, res) {
-    // Save the mail
-    const savedMail = MessageController.savedMail(req, res);
+    const dateTime = Date.now();
 
-    // Send the mail
-    req.body.id = savedMail.data[0].id;
-    const sentMail = MessageController.sendDraftUtility(req, res);
-    res.status(201).json(sentMail);
+    // Save the mail
+    let values = [req.body.subject, req.body.message, UserController.user.getId(), dateTime, 'sent'];
+    db.addMessage(values).then((rows) => {
+      const [savedMail] = rows;
+      const mailId = savedMail.id;
+      const { subject, message, receiverId } = req.body;
+      values = [mailId, UserController.user.getId(), 'sent', dateTime];
+
+      // insert into sents table
+      db.insertSents(values).then((rows2) => {
+        if (rows2.length === 1) {
+        // insert into the inboxes table
+          values = [mailId, receiverId, 'unread', dateTime];
+          db.insertInboxes(values).then(() => res.status(201).json({
+            status: 201,
+            data: [{
+              id: mailId,
+              createdOn: dateTime,
+              subject,
+              message,
+              parentMessageId: null,
+              status: 'sent',
+            }],
+          }));
+        }
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
+    });
   }
 
   static saveDraft(req, res) {
-    const savedMail = MessageController.savedMail(req);
-    res.status(201).send(savedMail);
-  }
-
-  static savedMail(req) {
-    const mail = UserController.user.createMail({
-      subject: req.body.subject,
-      message: req.body.message,
-      receiverId: req.body.receiverId,
+    const message = [
+      req.body.subject,
+      req.body.message,
+      UserController.user.getId(),
+      Date.now(),
+      'draft',
+    ];
+    db.addMessage(message).then((rows) => {
+      const [mail] = rows;
+      res.status(201).json({
+        status: 201,
+        data: [{
+          id: mail.id,
+          createdOn: mail.date_time,
+          subject: mail.subject,
+          message: mail.message,
+          parentMessageId: null,
+          status: mail.status,
+        }],
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
-
-    return {
-      status: 201,
-      data: [{
-        id: mail.getId(),
-        createdOn: mail.getCreationDateTime(),
-        subject: mail.getSubject(),
-        message: mail.getMessage(),
-        parentMessageId: mail.getParentMessageId(),
-        status: mail.getStatus(),
-      }],
-    };
   }
 
   static sendDraft(req, res) {
-    const sentMail = MessageController.sendDraftUtility(req);
-    res.status(201).json(sentMail);
-  }
-
-  static sendDraftUtility(req) {
     const mailId = parseInt(req.body.id, 10);
-    const mail = Message.getMails(mailId)[0];
-    mail.setSubject(req.body.subject);
-    mail.setMessage(req.body.message);
+    const { subject, message, receiverId } = req.body;
+    const dateTime = Date.now();
+    let values = [subject, message, 'sent', mailId];
 
-    UserController.user.sendMail({
-      message: mail,
-      toUserId: parseInt(req.body.toUserId, 10),
+    db.updateMessage(values).then((rowCount) => {
+      if (rowCount === 1) {
+        // insert into sents table
+        values = [mailId, UserController.user.getId(), 'sent', dateTime];
+        db.insertSents(values).then((rows) => {
+          if (rows.length === 1) {
+            // insert into the inboxes table
+            values = [mailId, receiverId, 'unread', dateTime];
+            db.insertInboxes(values).then(() => res.status(200).json({
+              status: 200,
+              data: [{
+                id: mailId,
+                createdOn: dateTime,
+                subject,
+                message,
+                parentMessageId: null,
+                status: 'sent',
+              }],
+            }));
+          }
+        });
+      }
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
-    const [mailObj] = Message.getMails(req.body.id);
-    return {
-      status: 201,
-      data: [{
-        id: mailObj.getId(),
-        createdOn: mailObj.getCreationDateTime(),
-        subject: mailObj.getSubject(),
-        message: mailObj.getMessage(),
-        parentMessageId: mailObj.getParentMessageId(),
-        status: mailObj.getStatus(),
-      }],
-    };
   }
 
   static getInbox(req, res) {
-    res.status(200).json({
-      status: 200,
-      data: UserController.user.inbox(),
+    db.getInboxes().then((mails) => {
+      let inbox = mails.filter(mail => mail.receiver_id === UserController.user.getId());
+      inbox = inbox.map(mail => ({
+        id: mail.msg_id,
+        createdOn: new Date(parseInt(mail.date_time, 10)).toLocaleString('en-US', { timeZone: 'UTC' }),
+        subject: mail.subject,
+        message: mail.message,
+        senderId: mail.owner_id,
+        receiverId: mail.receiver_id,
+        parentMessageId: null,
+        status: mail.status,
+      }));
+      res.status(200).json({
+        status: 200,
+        data: inbox,
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
   }
 
   static getUnreadInbox(req, res) {
-    res.status(200).json({
-      status: 200,
-      data: UserController.user.unReadInbox(),
+    db.getInboxes().then((mails) => {
+      let inbox = mails.filter(mail => mail.receiver_id === UserController.user.getId() && mail.status === 'unread');
+      inbox = inbox.map(mail => ({
+        id: mail.msg_id,
+        createdOn: new Date(parseInt(mail.date_time, 10)).toLocaleString('en-US', { timeZone: 'UTC' }),
+        subject: mail.subject,
+        message: mail.message,
+        senderId: mail.owner_id,
+        receiverId: mail.receiver_id,
+        parentMessageId: null,
+        status: mail.status,
+      }));
+      res.status(200).json({
+        status: 200,
+        data: inbox,
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
   }
 
   static getReadInbox(req, res) {
-    res.status(200).json({
-      status: 200,
-      data: UserController.user.readInbox(),
+    db.getInboxes().then((mails) => {
+      let inbox = mails.filter(mail => mail.receiver_id === UserController.user.getId() && mail.status === 'read');
+      inbox = inbox.map(mail => ({
+        id: mail.msg_id,
+        createdOn: new Date(parseInt(mail.date_time, 10)).toLocaleString('en-US', { timeZone: 'UTC' }),
+        subject: mail.subject,
+        message: mail.message,
+        senderId: mail.owner_id,
+        receiverId: mail.receiver_id,
+        parentMessageId: null,
+        status: mail.status,
+      }));
+      res.status(200).json({
+        status: 200,
+        data: inbox,
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
   }
 
   static getSentMail(req, res) {
-    res.status(200).json({
-      status: 200,
-      data: UserController.user.getSentMail(),
+    db.getSents(UserController.user.getId()).then((mails) => {
+      const sent = mails.map(mail => ({
+        id: mail.id,
+        createdOn: new Date(parseInt(mail.date_time, 10)).toLocaleString('en-US', { timeZone: 'UTC' }),
+        subject: mail.subject,
+        message: mail.message,
+        senderId: mail.owner_id,
+        receiverId: mail.receiver_id,
+        parentMessageId: null,
+        status: 'sent',
+      }));
+
+      res.status(200).json({
+        status: 200,
+        data: sent,
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
   }
 
   static getMailId(req, res) {
-    res.status(200).json({
-      status: 200,
-      data: Message.getMails(parseInt(req.params.id, 10)),
+    db.getInboxes().then((mails) => {
+      const mailId = parseInt(req.params.id, 10);
+      let inbox = mails.filter(mail => mail.receiver_id === UserController.user.getId()
+      && mail.msg_id === mailId);
+      inbox = inbox.map(mail => ({
+        id: mail.msg_id,
+        createdOn: new Date(parseInt(mail.date_time, 10)).toLocaleString('en-US', { timeZone: 'UTC' }),
+        subject: mail.subject,
+        message: mail.message,
+        senderId: mail.owner_id,
+        receiverId: mail.receiver_id,
+        parentMessageId: null,
+        status: mail.status,
+      }));
+      res.status(200).json({
+        status: 200,
+        data: inbox,
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
   }
 
   static deleteMail(req, res) {
     const mailId = parseInt(req.params.id, 10);
-    const mailMessage = Message.getMails(mailId)[0].getMessage();
-    // Delete mail
-    UserController.user.deleteMail(mailId);
-    res.status(200).json({
-      status: 200,
-      data: [{
-        message: mailMessage,
-      }],
+    db.deleteMessage(mailId, req.deleteType).then(() => {
+      db.getMessages(mailId).then((rows2) => {
+        res.status(200).json({
+          status: 200,
+          data: [{
+            message: rows2[0].message,
+          }],
+        });
+      });
+    }).catch((err) => {
+      const errorMessage = `SERVER ERROR: ${err.message}`;
+      Utility.handleError(res, errorMessage, 500);
     });
   }
 }
